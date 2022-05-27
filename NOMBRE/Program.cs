12cd5735c758bdf7bf2ab4c48a2744a84595da24
Ximena Valenzuela <xduarte@uabc.edu.mx>
2022-05-27 00:07:34 -0700

    Agregar archivos de proyecto.

---
+++ NOMBRE/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOMBRE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ximena Duarte Valenzuela");
+            Console.WriteLine("Mexicali, Baja California");
+            Console.WriteLine("08 de Mayo de 2000");
+            Console.ReadKey();
+
+        }
+    }
+}
